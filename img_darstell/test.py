48cb2eb2aac746ebba68c6040362165b85f986fa
Erfan Golchini <erfan.golchini@bwedu.de>
2018-08-18 16:13:41 +0200

    neues Repo

---
+++ img_darstell/test.py
@@ -0,0 +1,20 @@
+#Import libraries
+from PIL import Image
+from PIL import ImageDraw
+ 
+#Image Size
+EPD_WIDTH       = 176
+EPD_HEIGHT      = 264
+# Create a white mask 
+mask = Image.new('1', (EPD_HEIGHT,EPD_WIDTH), 255)   
+#Create a Draw object than allows to add elements (line, text, circle...) 
+draw = ImageDraw.Draw(mask)
+#Some Text
+draw.text((EPD_HEIGHT/4,EPD_WIDTH/2), 'Ali ist ein Hurensohn', fill = 0)
+#Horizontal line
+draw.line((0,EPD_WIDTH/2 + 12, EPD_HEIGHT, EPD_WIDTH/2 + 12), fill = 0)
+#Save the picture on disk
+neu = Image.new('1',(EPD_WIDTH, EPD_HEIGHT),255)
+neu = mask.transpose(Image.ROTATE_90)
+neu.show()
+neu.save('demopill.bmp',"bmp")
